043dc1a6f4daa31967a809d815dde27427a09d81
Arkham <wuxin.zju@qq.com>
2018-06-14 14:35:38 +0800

    add

---
+++ Project/PLC编程/wuxin/IndraLogic_L20_DP/_Emb_Logic/indralogic_l20_dp.cmd
@@ -0,0 +1,12 @@
+replace query 
+file open "C:\Users\Admin\Desktop\wuxin\wuxin\IndraLogic_L20_DP\_Emb_Logic\IndraLogic_L20_DP.pro"
+query off ok
+gateway local
+device name "Tcp/Ip (Level 2 Route)"
+device parameter 1001 192.168.1.20
+device parameter 1000 1200
+device parameter 100 No
+device parameter 1020 0
+device instance Local_
+query off ok
+query on
---
+++ Project/PLC编程/wuxin/IndraLogic_L20_DP/indralogic_l20_dp.ILD.di
@@ -0,0 +1,11 @@
+﻿<?xml version="1.0" encoding="UTF-8"?>
+<DiagnosisCompileData ChangeId="0" Guid="542809d6-f581-42c4-b938-084fb2531666" DataFormat="2.4.0">
+  <DeletedIds>
+    <Change ID="0" />
+  </DeletedIds>
+  <Poes />
+  <DiagServerData />
+  <DiagServerInit />
+  <GlobalMachineState />
+  <GlobalVariables />
+</DiagnosisCompileData>
